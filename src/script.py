5d5afabad504d4c3d115c37cf0b006d131b69de2
mkarkour <143955961+mkarkour@users.noreply.github.com>
2024-03-31 18:44:31 +0200

    editing Extract class second level

--- src/script.py
+++ src/script.py
@@ -1,12 +1,23 @@
-from module.notify import Sender
 import json
+import logging
 
+import module.setup_logging
+from module.notify import Sender
+from module.scrapping import Extract
 
-with open("credentials.json") as f:
-    creds = json.load(f)
-sender = Sender(creds.get('email_address'), creds.get('password'))
-sender.send("karmehdi@hotmail.com", "test")
-print("done")
+# logger = logging.getLogger(__name__)
+# logger.info("Starting")
+
+# with open("credentials.json") as f:
+#     creds = json.load(f)
+# sender = Sender(creds.get('email_address'), creds.get('password'))
+# sender.send("karmehdi@hotmail.com", "test")
+# print("done")
+
+extract = Extract()
+to_scrap = extract.get_links(1, 20)
+test = extract.parse_content(to_scrap[0])
+print(len(test))
 
 if __name__ == "__main__":
     # main()
--- test/test_module.py
+++ test/test_module.py
@@ -1,6 +1,25 @@
 import pytest
+
 from src.module.scrapping import Extract
 
 
-def test_get_links_of_extract():
-    assert len(Extract(1, 636).get_links()) == 635
+@pytest.fixture
+def extract_instance():
+    return Extract()
+
+
+@pytest.fixture
+def extract_links():
+    return Extract().get_links(1, 2)
+
+
+def test_get_links_of_extract(extract_instance):
+    assert len(extract_instance(1, 636).get_links()) == 635
+
+
+def test_type_get_links_of_extract(extract_instance):
+    assert type(extract_instance.get_links()) is type(list())
+
+
+def test_parse_content_of_extract(extract_instance, extract_links):
+    assert len(extract_instance.parse_content(extract_links[0])) == 25
